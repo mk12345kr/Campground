b99941184f46214186b3b126b3a9c9f6c2083632
mk12345kr <mk12345kr@gmail.com>
2020-04-23 10:59:16 +0530

    add flash success and error message

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
+    flash       = require("connect-flash"),
     passport    = require("passport"),
     LocalStrategy = require("passport-local"),
     methodOverride = require("method-override"),
@@ -9,24 +10,25 @@ var express     = require("express"),
     Comment     = require("./models/comment"),
     User        = require("./models/user"),
     seedDB      = require("./seeds")
-
+    
 //requiring routes
 var commentRoutes    = require("./routes/comments"),
-campgroundRoutes = require("./routes/campgrounds"),
-indexRoutes      = require("./routes/index")
-
-mongoose.connect("mongodb://localhost/yelp_camp_v10");
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
+    
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+app.use(flash());
 // seedDB(); //seed the database
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
-secret: "Once again Rusty wins cutest dog!",
-resave: false,
-saveUninitialized: false
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -35,8 +37,10 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
-res.locals.currentUser = req.user;
-next();
+   res.locals.currentUser = req.user;
+   res.locals.error = req.flash("error");
+   res.locals.success = req.flash("success");
+   next();
 });
 
 app.use("/", indexRoutes);
